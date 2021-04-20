1e874fe033b380cc668bfe0e000428151d150ca3
MoogleTroupe <ffxivmusicbard@gmail.com>
2021-04-20 11:42:07 -0400

    move secondary exe out of dll into second download. sign the exe.

--- FFBardMusicPlayerInit/BmpProgram.cs
+++ FFBardMusicPlayerInit/BmpProgram.cs
@@ -1,22 +1,28 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Net;
 using System.Windows.Forms;
 
 namespace FFBardMusicPlayer
 {
     public class Program
     {
+        internal const string downloadPath = @"https://dl.bardmusicplayer.com/bmp/legacy/lib/";
         internal static string localPath = Application.CommonAppDataPath + @"\";
         public void StartUp(int memoryVersion, string titleVersion, string messageText)
         {
-            localPath = localPath + @"FFBardMusicPlayerInternal.exe";
-
-            File.WriteAllBytes(localPath, FFBardMusicPlayerInit.Properties.Resources.FFBardMusicPlayerInternal);
-            ProcessStartInfo startInfo = new ProcessStartInfo(localPath)
+            using (var webClient = new WebClient())
             {
-                Arguments = memoryVersion + " \"" + titleVersion + "\" \"" + messageText + "\""
-            };
-            Process.Start(startInfo);
+                if (File.Exists(localPath + @"FFBardMusicPlayerInternal.exe") && !"FA66E7F67CD2E04439D6F46B99504EE7E8EDC096FBBBD485541BB2B26CB6A1DF".ToLower().Equals(Sha256.GetChecksum(localPath + @"FFBardMusicPlayerInternal.exe").ToLower()))
+                    webClient.DownloadFile(new Uri(downloadPath + @"FFBardMusicPlayerInternal.exe"), localPath + @"FFBardMusicPlayerInternal.exe");
+                
+                ProcessStartInfo startInfo = new ProcessStartInfo(localPath + @"FFBardMusicPlayerInternal.exe")
+                {
+                    Arguments = memoryVersion + " \"" + titleVersion + "\" \"" + messageText + "\""
+                };
+                Process.Start(startInfo);
+            }
         }
     }
 }
