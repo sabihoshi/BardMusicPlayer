f356f8319eb82f401a6981ab4d9c9f6755f46000
MoogleTroupe <ffxivmusicbard@gmail.com>
2021-04-21 00:06:14 -0400

    hash update for new bmp 1.3 build

--- FFBardMusicPlayerInit/BmpProgram.cs
+++ FFBardMusicPlayerInit/BmpProgram.cs
@@ -14,7 +14,7 @@ namespace FFBardMusicPlayer
         {
             using (var webClient = new WebClient())
             {
-                if (!(File.Exists(localPath + @"FFBardMusicPlayerInternal.exe") && "FA66E7F67CD2E04439D6F46B99504EE7E8EDC096FBBBD485541BB2B26CB6A1DF".ToLower().Equals(Sha256.GetChecksum(localPath + @"FFBardMusicPlayerInternal.exe").ToLower())))
+                if (!(File.Exists(localPath + @"FFBardMusicPlayerInternal.exe") && "557D27116764D5D2F60C72DB77AE7AE0D374B6F7B06B0D2E08AAB92FFDBAF4B2".ToLower().Equals(Sha256.GetChecksum(localPath + @"FFBardMusicPlayerInternal.exe").ToLower())))
                     webClient.DownloadFile(new Uri(downloadPath + @"FFBardMusicPlayerInternal.exe"), localPath + @"FFBardMusicPlayerInternal.exe");
                 
                 ProcessStartInfo startInfo = new ProcessStartInfo(localPath + @"FFBardMusicPlayerInternal.exe")
